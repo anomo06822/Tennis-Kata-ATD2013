c8c62ca891ee29f01b51a2a23a856daf1542e7d8
Peter Kofler <peter.kofler@code-cop.org>
2013-10-22 22:52:56 +0200

    format Ruby code (only whitespace changes)

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -1,4 +1,3 @@
-
 class TennisScorer
 
   SCORE_NAMES =  {
@@ -9,46 +8,45 @@ class TennisScorer
   }
 
   def initialize(player1Name, player2Name, p1points, p2points)
-   @player1Name = player1Name
-   @player2Name = player2Name
-   @p1points = p1points
-   @p2points = p2points
- end
+    @player1Name = player1Name
+    @player2Name = player2Name
+    @p1points = p1points
+    @p2points = p2points
+  end
 
-# options: extract bodies, extract conditions with name, extract maps to constants
- def score
-   result = ""
-   tempScore=0
-   if same_score?
-     if at_least_4_points?
-       result = "Deuce"
-     else
-       result = SCORE_NAMES[@p1points] + "-All"
-     end
-   elsif at_least_4_points?
-     minusResult = @p1points-@p2points
-     if (minusResult==1)
-       result ="Advantage " + @player1Name
-     elsif (minusResult ==-1)
-       result ="Advantage " + @player2Name
-     elsif (minusResult>=2)
-       result = "Win for " + @player1Name
-     else
-       result ="Win for " + @player2Name
-     end
-   else
-     (1...3).each do |i|
-       if (i==1)
-         tempScore = @p1points
-       else
-         result+="-"
-         tempScore = @p2points
-       end
-       result += SCORE_NAMES[tempScore]
-     end
-   end
-   result
- end
+  def score
+    result = ""
+    tempScore=0
+    if same_score?
+      if at_least_4_points?
+        result = "Deuce"
+      else
+        result = SCORE_NAMES[@p1points] + "-All"
+      end
+    elsif at_least_4_points?
+      minusResult = @p1points-@p2points
+      if (minusResult==1)
+        result ="Advantage " + @player1Name
+      elsif (minusResult ==-1)
+        result ="Advantage " + @player2Name
+      elsif (minusResult>=2)
+        result = "Win for " + @player1Name
+      else
+        result ="Win for " + @player2Name
+      end
+    else
+      (1...3).each do |i|
+        if (i==1)
+          tempScore = @p1points
+        else
+          result+="-"
+          tempScore = @p2points
+        end
+        result += SCORE_NAMES[tempScore]
+      end
+    end
+    result
+  end
 
   private
 
@@ -83,7 +81,7 @@ class TennisGame
     end
   end
 
-  # move method to another class
+  # TODO move method to another class
   def score
     TennisScorer.new(@player1Name, @player2Name, @p1points, @p2points).score
   end
